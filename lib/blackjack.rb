8b9d94a62a5246c050fb1bb104e3c178ce5211c7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-12-06 15:55:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,8 @@ def end_game(card_total)
 end
 
 def initial_round
- deal_card + deal_card
+ sum = deal_card + deal_card
+return sum
 # binding.pry
 # puts "Your cards add up to #{display_card_total} "
 end
