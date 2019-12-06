9b614792bed1996022255807c7c95fc89650a759
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-12-06 16:44:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ end
 
 def hit?(card_total)
   prompt_user
-  user_input - get_user_input
+  user_input = get_user_input
   if get_user_input == "h"
     card_total += deal_card
     while user_input != "h" && user_input != "s"
