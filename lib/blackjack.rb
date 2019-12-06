0723095db202c449c47f5ee814806d89f1f9e3ee
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-12-06 16:11:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,8 +8,8 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total(num)
-puts "Your cards add up to #{num}"
+def display_card_total(card_total)
+puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
