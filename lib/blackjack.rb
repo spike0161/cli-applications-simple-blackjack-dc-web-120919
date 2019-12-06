7c22f7759af2b8052e07d4d1fc63e3d3af7a94d2
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-12-06 15:35:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,7 +22,7 @@ def end_game(card_total)
  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round
+def initial_round(total)
    total = deal_card + deal_card
    total = display_card_total
    puts "Your cards add up to #{total}"
